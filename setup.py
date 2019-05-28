b27ae42f6d7b0157860878fafba8aa17cfcc8a4f
Gustav Wengel <gustavwengel@gmail.com>
2019-05-28 07:32:47 +0200

    added new version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/GeeWee/django-auto-prefetching"
 EMAIL = "gustavwengel@gmail.com"
 AUTHOR = "Gustav Wengel"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["django ~= 2.2"]
