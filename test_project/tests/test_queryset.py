c44fe22133509aa98eb0c83503cddc2d41a12ed3
Gustav Wengel <gustavwengel@gmail.com>
2019-09-05 10:00:37 +0200

    added one to one test

--- test_project/tests/test_queryset.py
+++ test_project/tests/test_queryset.py
@@ -1,7 +1,8 @@
+from django.db.models import QuerySet
 from django.test import TestCase
 
 from django_auto_prefetching.queryset_trace import TracingQuerySet
-from test_project.models import ChildB, TopLevel
+from test_project.models import ChildB, TopLevel, ChildABro, ChildA
 
 
 class TestTracingQuerySet(TestCase):
@@ -37,6 +38,42 @@ class TestTracingQuerySet(TestCase):
                 print('--------->', i.parent)
                 print('---------> printing i parent AGAIN:')
                 print('--------->', i.parent)
+
+    def test_tracing_queryset_will_fetch_one_to_one_fields_directly_on_the_model(self):
+        for i in range(0, 10):
+            child_a = ChildA.objects.create(
+                childA_text=i
+            )
+            ChildABro.objects.create(
+                brother_text=i,
+                sibling = child_a
+            )
+
+        # Here we should get only three queries on both sides.
+        # 1. For the .all call
+        # 2. For the .brother/sibling lazyload
+        # 3. One for the internal .all() call with the prefetched parents
+        with self.assertNumQueries(3):
+            qs = TracingQuerySet(model=ChildA)
+            for i in qs:
+                print('---------> printing i:')
+                print('--------->', i)
+                print('---------> printing i parent:')
+                print('--------->', i.brother)
+                print('---------> printing i parent AGAIN:')
+                print('--------->', i.brother)
+
+        with self.assertNumQueries(3):
+            qs = TracingQuerySet(model=ChildABro)
+
+            for i in qs:
+                print('---------> printing i:')
+                print('--------->', i)
+                print('---------> printing i parent:')
+                print('--------->', i.sibling)
+                print('---------> printing i parent AGAIN:')
+                print('--------->', i.sibling)
+
 
     def test_tracing_queryset_will_fetch_one_to_many_fields_directly_on_the_model(self):
         for i in range(0, 10):
